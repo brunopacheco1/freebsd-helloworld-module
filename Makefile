357ce8c52a72b31101f6eb1f0f649cf6e60946e9
Bruno Pacheco <brunopacheco1@yahoo.com>
2024-10-21 22:33:04 +0200

    feat: add hello_world function

---
+++ Makefile
@@ -0,0 +1,5 @@
+SRCS=hello_kld.c
+KMOD=hello_kld
+
+.include <bsd.kmod.mk>
+
---
+++ hello_kld.c
@@ -0,0 +1,29 @@
+#include <sys/param.h>
+#include <sys/kernel.h>
+#include <sys/module.h>
+#include <sys/systm.h>
+
+static int hello_loader(struct module *m, int event, void *arg)
+{
+    switch(event)
+    {
+        case MOD_LOAD:
+            uprintf("hello_kld - Hello World\n");
+            break;
+        case MOD_UNLOAD:
+            uprintf("hello_kld - Goodbye, Kernel\n");
+            break;
+        default:
+            return EOPNOTSUPP;
+    }
+    return 0;
+}
+
+static moduledata_t hello_mod = Â{
+    "Hello World Kernel Module",
+    hello_loader,
+    NULL,
+};
+
+DECLARE_MODULE(hello_world, hello_mod, SI_SUB_KLD, SI_ORDER_ANY);
+
